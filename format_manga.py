a75952d6ee56589d62a8cb3103f3f338e04a57ac
unknown <b10200092@yuntech.org.tw>
2020-01-04 20:31:36 +0800

    Modify usage of foramt_manga 1. Add index automatically 2. Rename existing folder if it was the only one of the author

--- format_manga.py
+++ format_manga.py
@@ -1,32 +1,97 @@
 import re
 import os
 
-dir_path = r"D:\\N"
-os.chdir(dir_path)
+dir_path = r'D:\\N'
+storage_path = r'G:\\EX-II\\漫'
 
-for folder_name in os.listdir(dir_path):
-    if not os.path.isdir(os.path.join(dir_path, folder_name)):
-        continue
-    
-    if not re.search("^(\(.*?\))? ?\[.*?\].*?$", folder_name):
-        continue
-		
-    new_name = re.sub(" ?((\[(\d{6,7}|DL.|別.*)\])|( \+ .*))*$", "", folder_name, flags=re.IGNORECASE)
-    info = re.sub("\[\d{6,7}\]$", "", folder_name.replace(new_name, ""))
-    
-    new_name = re.sub("^(\(.*?\))? ?\[(?P<author>.*?)\] ?(?P<name>.*?)$",
-                        "\g<author>　()\g<name>",
-                        new_name,
-                        flags=re.IGNORECASE)\
-		 .strip()
-						
-    while os.path.isdir(new_name) :
-        if info != "":
-            new_name += info
-            info = ""
+def main():
+    manga_list = os.listdir(storage_path)
+
+    for folder_name in os.listdir(dir_path):
+        # Ignore non-folder.
+        if not os.path.isdir(os.path.join(dir_path, folder_name)):
+            continue
+
+        # Ignore folder which is not matched.
+        if not re.search('^(\(.*?\))? ?\[.*?\].*?$', folder_name):
+            continue
+
+        folder_name_without_info, info = split_folder_name_and_info(folder_name)
+        author, title = get_author_and_title(folder_name_without_info)
+        existing_mangas = [folder_name for folder_name in manga_list if folder_name.startswith(author + '　')]
+        new_index = 0
+
+        for existing_folder_name in existing_mangas:
+            existing_folder_name_temp = re.sub('\(\d+\)', "", existing_folder_name)
+            existing_author, existing_title = existing_folder_name_temp.split('　', 1)
+            # If the folder is existing in storage_path
+            if existing_author == author and existing_title == title:
+                new_folder_name = existing_folder_name + ' [another]'
+                break
         else:
-            new_name += " [another]"
-    print(folder_name.ljust(40), "\t->", new_name)
-    os.rename(folder_name, new_name)
+            new_index = get_new_index(existing_mangas)
+            new_folder_name = get_new_folder_name(author, title, new_index)
 
-os.system("pause")
+        new_folder_name = add_info_if_duplicate(new_folder_name, info)
+        
+        rename_folder(folder_name, new_folder_name, dir_path)
+        rename_existing_folder(new_index, existing_mangas)
+
+    os.system('pause')
+
+def split_folder_name_and_info(folder_name):
+    folder_name_without_info = re.sub(' ?((\[(\d{6,7}|DL.|別.*)\])|( \+ .*))*$', '', folder_name, flags=re.IGNORECASE)
+    info = re.sub('\[\d{6,7}\]$', '', folder_name.replace(folder_name_without_info, ''))
+    return folder_name_without_info, info
+
+
+def get_author_and_title(folder_name):
+    match = re.match('^(\(.*?\))? ?\[(?P<author>.*?)\] ?(?P<title>.*?)$',
+        folder_name,
+        flags=re.IGNORECASE)
+    return match.group('author'), match.group('title')
+
+
+def get_new_index(existing_mangas):
+    indexes = []
+    for folder_name in existing_mangas:
+        search = re.search('　\((\d+)\)', folder_name);
+        if search:
+            index = int(search.group(1))
+            indexes.append(index)
+
+    if not existing_mangas:
+        return 0
+    if not indexes:
+        return 2
+    else:
+        return max(indexes) + 1
+
+
+def get_new_folder_name(author, title, new_index):
+    if new_index == 0:
+        return f'{author}　{title}'
+    else:
+        return f'{author}　({new_index}){title}'
+
+
+def add_info_if_duplicate(new_folder_name, info):
+    while os.path.isdir(new_folder_name) :
+        if info != '':
+            new_folder_name += info
+            info = ''
+        else:
+            new_folder_name += ' [another]'
+    return new_folder_name
+
+def rename_folder(old_name, new_name, path):
+    os.chdir(path)
+    print(old_name.ljust(40), '\t->', new_name)
+    #os.rename(old_name, new_name)
+
+def rename_existing_folder(new_index, existing_mangas):
+    if new_index == 2:
+        author, title = existing_mangas[0].split('　', 1)
+        rename_folder(existing_mangas[0], get_new_folder_name(author, title, 1), storage_path)
+
+main()
