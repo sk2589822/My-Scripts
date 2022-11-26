9d899e55a643cfa20e4da3b75bd15417e85e5902
JK <sk25898222@gmail.com>
2022-11-26 21:52:37 +0800

    allow input multiple paths

--- resize_with_ImageMagick.ps1
+++ resize_with_ImageMagick.ps1
@@ -1,17 +1,40 @@
-$path = Read-Host "Please enter path"
-$namePattern = Read-Host "Please enter pattern of of filesnames"
+Add-Type -AssemblyName Microsoft.VisualBasic
 
-Set-Location -LiteralPath $path
+while ($true) {
+    $inputPath = Read-Host "Please enter path"
+    if ($inputPath -eq '') {
+        break
+    }
 
-# move all files to to /Orignal
-New-Item -Path . -Name "Orignal" -ItemType "directory"
-Move-Item -Path .\*.* -Destination "Orignal"
+    $namePattern = Read-Host "Please enter pattern of of filesnames"
+    if ([string]::IsNullOrWhiteSpace($namePattern))
+    {
+        $namePattern = "*"
+    }
 
-# run magick
-$magickExePath = "C:\Program Files\ImageMagick-7.1.0-Q16-HDRI\magick.exe"
-$arguments = "-monitor", "./Orignal/$namePattern", "-resize", "x2160>", "-set", "filename:name", "%t", "$path/%[filename:name]-resized.jpg"
+    $paths = $inputPath.Split(",")
+    
+    foreach ($path in $paths)
+    {
+        Set-Location -LiteralPath $path
 
-&$magickExePath $arguments
+        # move matched files to to /Orignal
+        $originFolder = "./Orignal"
 
-Write-Host "Done!"
-[Console]::ReadKey()
+        if (-Not (Test-Path -Path $originFolder)) {
+            New-Item -Path . -Name "Orignal" -ItemType "directory"
+            Move-Item -Path .\$namePattern.* -Destination "Orignal"
+        }
+
+        # run magick
+        $magickExePath = "C:\Program Files\ImageMagick-7.1.0-Q16-HDRI\magick.exe"
+        $arguments = "-monitor", "./Orignal/*", "-resize", "x2160>", "-set", "filename:name", "%t", "$path/%[filename:name]-resized.jpg"
+
+        &$magickExePath $arguments
+		
+        [Microsoft.VisualBasic.FileIO.FileSystem]::DeleteDirectory("$path/Orignal" , "OnlyErrorDialogs", "SendToRecycleBin")
+    }
+    
+    Write-Host "Done!"
+}
+
