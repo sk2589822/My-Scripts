944880027383f48bd6df4d76971a47c2f5ee44ea
JK <sk25898222@gmail.com>
2023-02-24 23:26:48 +0800

    feat: add recursive option

--- resize_with_ImageMagick.ps1
+++ resize_with_ImageMagick.ps1
@@ -1,46 +1,56 @@
 Add-Type -AssemblyName Microsoft.VisualBasic
 
 while ($true) {
-    $inputPath = Read-Host "Please enter path"
-    if ($inputPath -eq '') {
-        break
+  $inputPath = Read-Host "Path of directory: "
+  if ($inputPath -eq '') {
+    break
+  }
+
+  $namePattern = Read-Host "Pattern of filesnames: "
+  if ([string]::IsNullOrWhiteSpace($namePattern)) {
+    $namePattern = "*"
+  }
+
+  $isRecursive = Read-Host "Recursive?(y/N)"
+
+  if ($isRecursive -eq 'y' || $isRecursive -eq 'Y') {
+    $paths = @(ls $inputPath)
+  } else {
+    $paths = New-Object System.Collections.ArrayList
+    foreach ($path in $inputPath.Split(",")) {
+      $paths.Add($path.Trim())
+    }
+    $paths = $paths.toArray()
+  }
+  
+  foreach ($path in $paths) {
+    if (!$path) {
+      continue
     }
 
-    $namePattern = Read-Host "Please enter pattern of filesnames"
-    if ([string]::IsNullOrWhiteSpace($namePattern)) {
-        $namePattern = "*"
+    Set-Location -LiteralPath $path
+
+    # move matched files to to /Orignal
+    $originFolder = "./Orignal"
+    if (-Not (Test-Path -Path $originFolder)) {
+      New-Item -Path . -Name "Orignal" -ItemType "directory"
+      Move-Item -Path .\$namePattern.* -Include "*.jpg", "*.jpeg", "*.png" -Destination "Orignal"
     }
 
-    $paths = $inputPath.Split(",")
-    
-    foreach ($path in $paths) {
-        if (!$path) {
-            continue
-        }
-		
-        Set-Location -LiteralPath $path.Trim()
+    # run magick
+    $magickExePath = "C:\Program Files\ImageMagick-7.1.0-Q16-HDRI\magick.exe"
+    $arguments = "-monitor", "./Orignal/*", "-resize", "x2160>", "-set", "filename:name", "%t", "./%[filename:name]-resized.jpg"
 
-        # move matched files to to /Orignal
-        $originFolder = "./Orignal"
-        if (-Not (Test-Path -Path $originFolder)) {
-            New-Item -Path . -Name "Orignal" -ItemType "directory"
-            Move-Item -Path .\$namePattern.* -Include "*.jpg", "*.jpeg", "*.png" -Destination "Orignal"
-        }
-
-        # run magick
-        $magickExePath = "C:\Program Files\ImageMagick-7.1.0-Q16-HDRI\magick.exe"
-        $arguments = "-monitor", "./Orignal/*", "-resize", "x2160>", "-set", "filename:name", "%t", "./%[filename:name]-resized.jpg"
-
-        try {
-            &$magickExePath $arguments
-        }
-        catch {
-            break
-        }
-		
-        [Microsoft.VisualBasic.FileIO.FileSystem]::DeleteDirectory("$path/Orignal" , "OnlyErrorDialogs", "SendToRecycleBin")
+    try {
+      &$magickExePath $arguments
     }
-    
-    Write-Host "Done!"
+    catch {
+      break
+    }
+
+    [Microsoft.VisualBasic.FileIO.FileSystem]::DeleteDirectory("$path/Orignal" , "OnlyErrorDialogs", "SendToRecycleBin")
+  }
+  
+  Write-Host "Done!"
 }
 
